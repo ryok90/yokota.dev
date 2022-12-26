3fef9b72703c773f1d1b997fc3ff9426c8f033fb
Yokota <ryok90@gmail.com>
2022-12-26 15:22:21 -0300

    feat: first commit

---
+++ src/theme.tsx
@@ -0,0 +1,18 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  primary: {
+    900: '#0A2647',
+    800: '#00305e',
+    700: '#174978',
+    600: '#2A475D',
+    500: '#2A475D',
+    400: '',
+    300: '',
+    200: '',
+    100: '',
+    50: '',
+  },
+}
+
+const theme = extendTheme({ colors })
